eb1cbaa4a2667b7d961fc551c7648fe4d58dbd27
hasansin <63632399+hasansin@users.noreply.github.com>
2021-05-07 20:56:55 +0530

    modal added to OderList

--- frontend/src/components/Order.js
+++ frontend/src/components/Order.js
@@ -1,32 +1,37 @@
 import { useEffect, useState } from 'react';
-import OrderItem from './OrderItem'
+
 import "./styles/Order.css";
+import "./styles/Popup.css"
+const Order = ({orders,id}) => {
 
-const Order = () => {
-
- const [items , setItems] = useState(null);  
- 
- 
- useEffect(()=>{
-     fetch('http://localhost:8000/orders')
-     .then(res => {
-         return res.json();
-     })
-     .then( data =>{
-        setItems(data);});
-
- },[]);
-
-
-    return ( 
+// const [items , setItems] = useState(null);  
+const order = orders.filter((order)=> id !== order.id);
+return ( 
 <div className="Order">
 
-  
- 
-  {items && <OrderItem items={items} title ="List"/>}
-   
-  
-</div> ); 
+{
+    <div className="item-list" key={order.id}>
+    <h1 style={{fontSize:"25px",float:"left",fontFamily: "Quicksand"}}>Order ID: 0111 {order.id}</h1><br />
+    
+    <br /><h1 style={{fontSize:"25px",float:"left",fontFamily: "Quicksand"}}>Address : {order.address}</h1><br />
+    
+    <br /><h1 style={{fontSize:"25px",float:"left",fontFamily: "Quicksand"}}>Date :{order.date} </h1>
+    
+
+        <div className="item" >   
+            <div className="text-content">
+                    <h1 style={{fontSize:"20px"}}>{order.name}</h1>
+                    <p >{order.description}</p>
+                    <p> </p>                   
+                    <p >Price : {order.price}</p>
+                    <p >quantity : {order.quantity}</p> 
+            </div>
+                <img src={order.mgurl} alt="picture"/>
+            </div> 
+            <div className="status" ><h4 >Status : {order.status}</h4></div>
+    </div>}
+    </div>
+    ); 
 }
     
  
--- frontend/src/components/OrderList.js
+++ frontend/src/components/OrderList.js
@@ -1,44 +1,93 @@
 import { Link } from "react-router-dom";
-import Order from "./Order";
 import "./styles/Order.css";
+import { useEffect, useState } from 'react';
+import Order from "./Order";
+import {Button,Modal} from 'react-bootstrap';
 
-const popup =() =>{
- <Link to="/order"></Link>
-}
+
+
+
+
 const OrderList = () => {
 
+    const [orders , setOrders] = useState(null);  
+  //  const [isClicked , setIsClicked] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  
+
+
+
+    useEffect(()=>{
+        fetch('http://localhost:8000/orders')
+        .then(res => {
+            return res.json();
+        })
+        .then( data =>{
+            console.log(data);
+            setOrders(data);}
+            );
+   
+    },[]);
+
+    const clicked = () => {
+        
+       <Link to="/order"></Link> }
+    
 
     return (  
     <div className="orderList">
       <h1 style={{textAlign:"center",padding:"20px"}}>User's Orders</h1>  
-    <table className="table">
-      <thead>
-          <th>Address</th>
-          <th>Order Id</th>
-          <th> Order Status</th>
-          <th> Seller</th>
-      </thead>
-      <tbody>
-          <tr className="row" onClick={popup}> 
-            <td>Kaluthra</td>
-            <td> 000001</td>
-            <td>Pending</td>
-            <td> Ranush</td>  
-          </tr>
-          <tr className="row"> 
-            <td>Kaluthra</td>
-            <td> 000001</td>
-            <td>Pending</td>
-            <td> Ranush</td>  
-          </tr>  
-          <tr className="row"> 
-            <td>Kaluthra</td>
-            <td> 000001</td>
-            <td>Pending</td>
-            <td> Ranush</td>  
-          </tr>   
-      </tbody>
-  </table>
+        <table className="table">
+            <thead>
+                <th>Address</th>
+                <th>Order Id</th>
+                <th> Order Status</th>
+                <th> Seller</th>
+                <th></th>
+            </thead>
+            {orders &&
+            <tbody>
+                {orders.map((order)=>(
+                
+                <tr className="row"  key={order.id}  > 
+                    <td>{order.location}</td>
+                    <td> {order.id}</td>
+                    <td>{order.status}</td>
+                    <td> {order.sellerId}</td>  
+                    <td><button className="bttn" onClick={handleShow} >Details</button>
+                    
+                      
+                        
+                   
+                    </td>
+
+                </tr>
+                ))}
+                
+            </tbody>
+            }
+            
+          
+
+        </table>
+                         <Modal ClassName="modal" show={show} onHide={handleClose}>
+                             <Modal.Header></Modal.Header>
+                            <Modal.Body>
+                            {orders && <Order orders={orders}  />} 
+                            <Button  onClick={handleClose}> x  </Button>
+                               
+                           
+                            </Modal.Body>
+                            
+                            
+                            
+                            
+                        </Modal>
+                        
               </div>
         );
 }
