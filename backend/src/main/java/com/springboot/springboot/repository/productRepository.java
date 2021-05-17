d78b4886f3b11c9aadf0fdb6fb617bfb26b51d18
jayasith <68503997+jayasith@users.noreply.github.com>
2021-05-17 20:55:07 +0530

    backend product update

--- backend/src/main/java/com/springboot/springboot/repository/productRepository.java
+++ backend/src/main/java/com/springboot/springboot/repository/productRepository.java
@@ -4,8 +4,10 @@ import com.springboot.springboot.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface productRepository extends MongoRepository<Product,String> {
 
     public List<Product> findBySellerId(String id);
+    public Optional<Product> findById(String id);
 }
