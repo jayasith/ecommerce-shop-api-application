e69f0884e3d212e43cac67066b81eaa0a42c8431
jayasith <68503997+jayasith@users.noreply.github.com>
2021-05-20 01:33:07 +0530

    payment and email

--- frontend/src/components/AddCardForm.js
+++ frontend/src/components/AddCardForm.js
@@ -5,25 +5,55 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Context from "./contexts/Context";
 
+
 function AddCardForm(props) {
   
   const context = useContext(Context);
   const products = [...context.products]
-  console.log(props.delivery);
- console.log(products);
+  const buyerid = context.userAuth
+  var email
+  
+  const getEmail = async()=>{
+   
+      const respons = await fetch(
+        `http://localhost:9090/rest/email/${buyerid}/`  
+      )
+       email = await respons.text();
 
+    }
+ 
+    getEmail();
 
   const date = new Date().getTime();
   console.log(date);
   const { city, state, streetaddress } = props.delivery;
-  const buyerid = context.userAuth;
 
   const [cardno, setCardno] = useState("");
   const [exdate, setExdate] = useState("");
   const [cvc, setCvc] = useState("");
 
+  const paymentconfirm = (serviceID, templateId, variables) => {
+    toast.success("Your payment successful");
+        window.emailjs.send(
+            serviceID, templateId,
+            variables
+        ).then(res => {
+            console.log('Email successfully sent!')
+        })
+            .catch(err => console.error('There has been an error.  Here some thoughts on the error that occured:', err))
+    }
+
+  const paymentemail = () => {
+        const templateId = 'template_3e10xcd';
+        const serviceID = 'service_f5upd53';
+        console.log(email);
+        paymentconfirm(serviceID, templateId, { to_name:" ", from_name: "shop.com", reply_to:email})
+      
+    }
+
   const cardhandle = async (e) => {
     e.preventDefault();
+    paymentemail();
     const card = { cardno, exdate, cvc };
     const order = { buyerid, date, city, products, state, streetaddress}
     console.log(order);
@@ -44,7 +74,9 @@ function AddCardForm(props) {
         setExdate("");
         setCvc("");
         toast.success("Your payment successful");
-        const orderResponse = await fetch(`http://localhost:9090/rest/`, {
+        
+
+        const orderResponse = await fetch(`http://localhost:9090/rest/card`, {
           method: "POST",
           headers: {
             Accept: "application/json",
--- frontend/src/components/AddNoForm.js
+++ frontend/src/components/AddNoForm.js
@@ -8,17 +8,48 @@ import Context from "./contexts/Context";
 function AddNoForm(props) {
    const context = useContext(Context);
   const products = [...context.products]
-  
+  const buyerid = context.userAuth
 
-  const { city, email, state, streetaddress } = props.delivery;
+  const { city, state, streetaddress } = props.delivery;
 
   const [send, setSend] = useState(true);
   const [mobileno, setMobileno] = useState("");
   const [code, setCode] = useState("");
+  
+  var email
+  
+  const getEmail = async()=>{
+   
+      const respons = await fetch(
+        `http://localhost:9090/rest/email/${buyerid}/`  
+      )
+       email = await respons.text();
 
-  const buyerid = context.userAuth;
+    }
+ 
+    getEmail();
+
+    const paymentconfirm = (serviceID, templateId, variables) => {
+    toast.success("Your payment successful");
+        window.emailjs.send(
+            serviceID, templateId,
+            variables
+        ).then(res => {
+            console.log('Email successfully sent!')
+        })
+            .catch(err => console.error('There has been an error.  Here some thoughts on the error that occured:', err))
+    }
+
+  const paymentemail = () => {
+        const templateId = 'template_3e10xcd';
+        const serviceID = 'service_f5upd53';
+        console.log(email);
+        paymentconfirm(serviceID, templateId, { to_name:" ", from_name: "shop.com", reply_to:email})
+      
+    }
 
   const codeHandle = async (e) => {
+    paymentemail();
     const mobile = { mobileno, code };
     const order = { buyerid, email,city, state,products, streetaddress };
     console.log(order);
