4ff919540ee10126b11597a7f789266dceb6a79c
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-05-21 21:53:00 +0530

    CREATE: protected routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,62 @@
 import { Route, Switch } from "react-router-dom";
+import { useContext } from "react";
 
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Form from "./components/Form";
 import Products from "./components/Products";
 import OrderList from "./components/OrderList";
-import Order from "./components/Order";
 import DeliveryForm from "./components/DeliveryForm";
 import AddItems from "./components/AddItems";
-import Payment from "./components/PaymentMethods";
 import ItemEditDelete from "./components/ItemEditDelete";
+import ErrorPage from "./components/ErrorPage";
+
+import Context from "./components/contexts/Context";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/buyer-register">
-          <Form fetchEndpoint="buyers" title="Register" isRegisterForm />
-        </Route>
-        <Route path="/seller-register">
-          <Form
-            fetchEndpoint="sellers"
-            title="Seller Register"
-            isRegisterForm
-          />
-        </Route>
-        <Route path="/login">
-          <Form fetchEndpoint="buyerlog" title="Login" isRegisterForm={false} />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path="/orders">
-          <OrderList />
-        </Route>
-        <Route path="/additems">
-          <AddItems isAddItem />
-        </Route>
-        <Route path="/payment">
-          <DeliveryForm isDeliveryForm />
-        </Route>
-        <Route path="/itemeditdelete">
-          <ItemEditDelete />
-        </Route>
-      </Switch>
-    </div>
-  );
+	const context = useContext(Context);
+
+	return (
+		<div className="App">
+			<Navbar />
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/buyer-register">
+					<Form fetchEndpoint="buyers" title="Register" isRegisterForm />
+				</Route>
+				<Route path="/seller-register">
+					<Form
+						fetchEndpoint="sellers"
+						title="Seller Register"
+						isRegisterForm
+					/>
+				</Route>
+				<Route path="/login">
+					<Form fetchEndpoint="buyerlog" title="Login" isRegisterForm={false} />
+				</Route>
+				<Route
+					path="/products"
+					component={context.userAuth ? Products : ErrorPage}
+				></Route>
+				<Route
+					path="/orders"
+					component={context.userAuth ? OrderList : ErrorPage}
+				></Route>
+				<Route path="/additems">
+					<AddItems isAddItem />
+				</Route>
+				<Route path="/payment">
+					<DeliveryForm isDeliveryForm />
+				</Route>
+				<Route path="/itemeditdelete">
+					<ItemEditDelete />
+				</Route>
+				<Route path="*" component={ErrorPage} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,83 +6,89 @@ import Cart from "./Cart";
 import Context from "./contexts/Context";
 
 const Navbar = () => {
-  const location = useLocation();
-  const [isToggle, setIsToggle] = useState(false);
+	const location = useLocation();
+	const [isToggle, setIsToggle] = useState(false);
 
-  const context = useContext(Context);
+	const context = useContext(Context);
 
-  return (
-    <header>
-      <div className="nav-container">
-        <h4 className="shop-name">SHOP</h4>
-        <nav>
-          <ul>
-            <li>
-              {location.pathname === "/products" ||
-              location.pathname === "/orders" ? (
-                <Link to="/products">Products</Link>
-              ) : (
-                <Link to="/">Home</Link>
-              )}
-            </li>
-            <li>
-              {location.pathname === "/products" ||
-              location.pathname === "/orders" ? (
-                <Link to="/orders">Orders</Link>
-              ) : (
-                ""
-              )}
-            </li>
-            {location.pathname === "/products" && (
-              <li
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  cursor: "pointer",
-                }}
-                onClick={() => {
-                  isToggle ? setIsToggle(false) : setIsToggle(true);
-                }}
-              >
-                <FaShoppingCart style={{ fontSize: "1.2rem" }} />
-                <p
-                  style={{
-                    marginLeft: ".6rem",
-                    fontSize: "0.8rem",
-                    padding: "0.2rem 0.5rem",
-                    borderRadius: "50%",
-                    background: "#ef233c",
-                    color: "white",
-                  }}
-                >
-                  {context.carts.length}
-                </p>
-              </li>
-            )}
-            <li>
-              {location.pathname === "/products" ||
-              location.pathname === "/orders" ? (
-                <Link to="/" className="important">
-                  Logout
-                </Link>
-              ) : (
-                <Link to="/seller-register" className="important">
-                  Register as Seller
-                </Link>
-              )}
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <Cart
-        isToggle={isToggle}
-        setIsToggle={setIsToggle}
-        carts={context.carts}
-        removeProductFromCart={context.removeProductFromCart}
-        clearCart={context.clearCart}
-      />
-    </header>
-  );
+	return (
+		<>
+			{context.userAuth ? (
+				<header>
+					<div className="nav-container">
+						<h4 className="shop-name">SHOP</h4>
+						<nav>
+							<ul>
+								<li>
+									{location.pathname === "/products" ||
+									location.pathname === "/orders" ? (
+										<Link to="/products">Products</Link>
+									) : (
+										<Link to="/">Home</Link>
+									)}
+								</li>
+								<li>
+									{location.pathname === "/products" ||
+									location.pathname === "/orders" ? (
+										<Link to="/orders">Orders</Link>
+									) : (
+										""
+									)}
+								</li>
+								{location.pathname === "/products" && (
+									<li
+										style={{
+											display: "flex",
+											alignItems: "center",
+											cursor: "pointer",
+										}}
+										onClick={() => {
+											isToggle ? setIsToggle(false) : setIsToggle(true);
+										}}
+									>
+										<FaShoppingCart style={{ fontSize: "1.2rem" }} />
+										<p
+											style={{
+												marginLeft: ".6rem",
+												fontSize: "0.8rem",
+												padding: "0.2rem 0.5rem",
+												borderRadius: "50%",
+												background: "#ef233c",
+												color: "white",
+											}}
+										>
+											{context.carts.length}
+										</p>
+									</li>
+								)}
+								<li>
+									{location.pathname === "/products" ||
+									location.pathname === "/orders" ? (
+										<Link to="/" className="important">
+											Logout
+										</Link>
+									) : (
+										<Link to="/seller-register" className="important">
+											Register as Seller
+										</Link>
+									)}
+								</li>
+							</ul>
+						</nav>
+					</div>
+					<Cart
+						isToggle={isToggle}
+						setIsToggle={setIsToggle}
+						carts={context.carts}
+						removeProductFromCart={context.removeProductFromCart}
+						clearCart={context.clearCart}
+					/>
+				</header>
+			) : (
+				""
+			)}
+		</>
+	);
 };
 
 export default Navbar;
